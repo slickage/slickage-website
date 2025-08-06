0f615759c983cf5f9d83b26c8a3ce57cfde5d980
Chris Rodrigues <crod951@gmail.com>
2025-08-05 17:30:22 -0700

    Successfully disabled PR merge because status check failed, reverting the formatting

--- src/components/contact-section.tsx
+++ src/components/contact-section.tsx
@@ -1,8 +1,7 @@
 'use client';
 
 import React from 'react';
-import { 
-  Send } from 'lucide-react';
+import { Send } from 'lucide-react';
 import { Button } from '@/components/ui/button';
 
 export default function ContactSection() {
