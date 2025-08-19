277313f759863a0e5e9fe2bb91a169a1db633710
Chris Rodrigues <crod951@gmail.com>
2025-08-18 21:33:19 -0700

    refactor: removing all unused pages and components

--- src/components/services/index.ts
+++
@@ -1,5 +0,0 @@
-export { default as ServiceCard } from './service-card';
-export { default as ServiceCta } from './service-cta';
-export { default as ServiceFeature } from './service-feature';
-export { default as ServiceGrid } from './service-grid';
-export { default as ServicesHero } from './services-hero';
