9130d6b021fc314561cee43f99d9019ccb214644
Chris Rodrigues <crod951@gmail.com>
2025-08-26 10:19:39 -0700

    chore: removed unused pexels url in next.config

--- next.config.ts
+++ next.config.ts
@@ -11,11 +11,6 @@ const nextConfig = {
         hostname: 'slickage-website.s3.us-west-2.amazonaws.com',
         pathname: '/images/**',
       },
-      {
-        protocol: 'https',
-        hostname: 'images.pexels.com',
-        pathname: '/photos/**',
-      },
     ],
     formats: ['image/webp', 'image/avif'],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
